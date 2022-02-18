bf7175bda8b8288b3346d59d07af6bf69703308a
Adil Marghady <68113808+adilmarghadi1@users.noreply.github.com>
2022-02-18 00:22:54 +0000

    Delete conn.php

--- conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$conn = new PDO('sqlite:db/employees.sqlite3');
- 
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
- 
-	$query = "CREATE TABLE IF NOT EXISTS student (student_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, firstname TEXT, lastname TEXT,date TEXT,département TEXT, salaire TEXT, fonction TEXT, photo filename)";
- 
-	$conn->exec($query);
-?>
